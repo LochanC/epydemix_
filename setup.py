988681988963820ddda4ad124bee6a3185dd8690
ngozzi <nic.gozzi@gmail.com>
2025-03-10 22:12:51 -0400

    new release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="epydemix",  
-    version="0.1.0",  
+    version="0.1.1",  
     author="Nicolò Gozzi",  
     author_email="epydemix@isi.it",  
     description="A Python package for epidemic modeling, simulation, and calibration",  
